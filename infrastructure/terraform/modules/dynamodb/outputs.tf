c2bab1a685c41f3361048fd56f111210b5a64156
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-03-27 19:03:11 -0700

    Create outputs.tf

---
+++ infrastructure/terraform/modules/dynamodb/outputs.tf
@@ -0,0 +1,19 @@
+output "table_name" {
+  description = "Name of the DynamoDB table"
+  value       = aws_dynamodb_table.this.name
+}
+
+output "table_arn" {
+  description = "ARN of the DynamoDB table"
+  value       = aws_dynamodb_table.this.arn
+}
+
+output "table_id" {
+  description = "ID of the DynamoDB table"
+  value       = aws_dynamodb_table.this.id
+}
+
+output "table_stream_arn" {
+  description = "ARN of the DynamoDB table stream"
+  value       = aws_dynamodb_table.this.stream_arn
+} 
