9f3f26a089abbde2c3b5fe47d802da07caa29ec2
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-03-27 19:03:05 -0700

    Create variables.tf

---
+++ infrastructure/terraform/modules/dynamodb/variables.tf
@@ -0,0 +1,36 @@
+variable "table_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+}
+
+variable "hash_key" {
+  description = "Hash key for the DynamoDB table"
+  type        = string
+}
+
+variable "attributes" {
+  description = "List of DynamoDB attributes"
+  type = list(object({
+    name = string
+    type = string
+  }))
+}
+
+variable "global_secondary_indexes" {
+  description = "List of global secondary indexes"
+  type = list(object({
+    name            = string
+    hash_key        = string
+    range_key       = string
+    projection_type = string
+    read_capacity   = number
+    write_capacity  = number
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "Tags to apply to the DynamoDB table"
+  type        = map(string)
+  default     = {}
+} 
